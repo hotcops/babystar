984c959f49542664cac10a29ba5440b20b012f8a
hotcops <shane.pizz@gmail.com>
2016-04-06 00:44:53 -0400

    Stripped 'tennis3' template to bare minimum, added simple navbar. Work on products page next, nice big photos, use bootstrap grid.

---
+++ public/scripts/app.js
@@ -0,0 +1,34 @@
+
+// Main Angular App
+angular.module('App', [])
+.controller('MainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log('This is helloWorld() working in mainCtrl!');
+  };
+})
+
+/* NOT CURRENTLY WORKING
+.directive('intensityBanner', function() {
+  return {
+    controller: 'mainCtrl',
+    templateUrl: 'public/views/intensityBanner.html'
+  };
+});
+*/
+
+
+
+
+// Slick Plugin Settings
+/*
+$(document).ready(function(){
+      $('.slideshow').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        centerMode: true,
+        infinite: true,
+        variableWidth: true
+      });
+    });
+*/
